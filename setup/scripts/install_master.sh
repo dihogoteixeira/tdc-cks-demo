2492b005e3607eb26b3dc942045d1cf770940df0
meli-diteixeira <dihogo.teixeira@mercadolivre.com>
2021-12-02 08:56:17 -0300

    adicionando README.md da demo

--- setup/scripts/install_master.sh
+++ setup/scripts/install_master.sh
@@ -134,6 +134,8 @@ kubeadm init --kubernetes-version=${KUBE_VERSION} --ignore-preflight-errors=NumC
 
 mkdir -p ~/.kube
 sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/
 
 # workaround because https://github.com/weaveworks/weave/issues/3927
 # kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
